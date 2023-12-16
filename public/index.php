65c6434d385dcfd38ab37ddda40e5f6de6104020
Kristian <10221425@ucn.dk>
2023-12-16 12:04:13 +0100

    Create a basic router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 require '../helpers.php';
-// require basePath('views/home.view.php');
 
-loadView('home');
+$routes = [
+  '/' => 'controllers/home.php',
+  '/listings' => 'controllers/listings/index.php',
+  '/listings/create' => 'controllers/listings/create.php',
+  '404' => 'controllers/error/404.php' // Add this route
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+// inspectAndDie($uri);
+
+if (array_key_exists($uri, $routes)) {
+  require basePath($routes[$uri]);
+} else {
+  require basePath($routes['404']);
+}
