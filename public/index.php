a6841f0ffe90a6b3eada852703712585761b6e91
Kristian <10221425@ucn.dk>
2023-12-17 08:53:19 +0100

    Router Refactor To Class

--- public/index.php
+++ public/index.php
@@ -1,7 +1,20 @@
 <?php
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-// inspectAndDie($uri);
+// require the router class
+require basePath('Router.php');
 
-require basePath('router.php');
+// instantiate the router object
+$router = new Router();
+
+// bring in the routes file
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+// inspect($uri);
+// inspect($method);
+
+// call the route method, pass in the uri & the method of whatever page we are visiting
+$router->route($uri, $method);
