f00b21cea3b938a52963d683c87c53d4a7342dc0
Kristian <10221425@ucn.dk>
2023-12-19 11:14:35 +0100

    Single Listing, Prepared Statements & Named Params

---
+++ controllers/listings/show.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = require basePath('config/_db.php');
+$db = new Database($config);
+
+$id = $_GET['id'] ?? '';
+// inspect($id);
+
+$params = [
+  // named params
+  'id' => $id
+];
+
+$listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+inspect($listing);
+
+// get the data from the database where that listing id matches the id in the url
+// load view and pass in the listing from the database, access them in the view with $listings
+loadView('listings/show');
